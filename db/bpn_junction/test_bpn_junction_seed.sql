a0594b0e642c33136c8e7b8a791ae6af8501303d
Sean White <sean.b.white89@icloud.com>
2018-09-05 15:54:23 -0600

    added test sql queries

---
+++ db/bpn_junction/test_bpn_junction_seed.sql
@@ -0,0 +1,60 @@
+INSERT INTO bpn_junction(article_id,topic_id,subtopic_id)
+VALUES(
+    1,
+    1,
+    1
+);
+INSERT INTO bpn_junction(article_id,topic_id,subtopic_id)
+VALUES(
+    2,
+    1,
+    1
+);
+INSERT INTO bpn_junction(article_id,topic_id,subtopic_id)
+VALUES(
+    3,
+    1,
+    2
+);
+INSERT INTO bpn_junction(article_id,topic_id,subtopic_id)
+VALUES(
+    4,
+    1,
+    2
+);
+INSERT INTO bpn_junction(article_id,topic_id,subtopic_id)
+VALUES(
+    5,
+    1,
+    3
+);
+INSERT INTO bpn_junction(article_id,topic_id,subtopic_id)
+VALUES(
+    6,
+    1,
+    3
+);
+INSERT INTO bpn_junction(article_id,topic_id,subtopic_id)
+VALUES(
+    7,
+    2,
+    4
+);
+INSERT INTO bpn_junction(article_id,topic_id,subtopic_id)
+VALUES(
+    8,
+    2,
+    4
+);
+INSERT INTO bpn_junction(article_id,topic_id,subtopic_id)
+VALUES(
+    9,
+    2,
+    5
+);
+INSERT INTO bpn_junction(article_id,topic_id,subtopic_id)
+VALUES(
+    11,
+    2,
+    5
+);
