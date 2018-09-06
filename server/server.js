bb315927116d2d9a96855c295851ce558d9fdd7a
mundix34 <mundix34@gmail.com>
2018-09-06 17:46:41 -0600

    git with all saved

--- server/server.js
+++ server/server.js
@@ -3,14 +3,10 @@ require("dotenv").config();
 
 const express = require("express"),
     bodyParser = require("body-parser"),
-<<<<<<< HEAD
     cors = require('cors'),
     massive = require("massive");
-=======
-    massive = require("massive")
     ArtCtrl = require('./controller/Articles')
     PlCtrl = require('./controller/Poll');
->>>>>>> master
 
 const app = express();
 const { SERVER_PORT, CONNECTION_STRING } = process.env;
@@ -21,19 +17,6 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // ENDPOINTS
-<<<<<<< HEAD
-app.get("/api/onload", async (req, res) => {
-    let db = app.get("db");
-    let result = await db.bpn_junction.test_bpn_query();
-    res.status(200).send(result);
-})
-app.get("/api/article/:topics", async (req, res) => {
-    let db = app.get("db");
-    let { topics } = req.params
-    let result = await db.articles.query_topics(topics)
-    res.status(200).send(result);
-})
-=======
 //Get 5 Random articles Onload
 app.get("/api/onload",ArtCtrl.art_onload)
 //Get 5 random articles based on user input
@@ -44,7 +27,6 @@ app.get("/api/topics",ArtCtrl.get_topics)
 app.get('/api/poll/results',PlCtrl.poll_res)
 //Add to Poll results
 app.post('/api/poll/submit',PlCtrl.poll_submit)
->>>>>>> master
 
 massive(CONNECTION_STRING).then(db => {
     app.set("db", db)
