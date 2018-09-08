72e0673c9a3f5e40dd300dd7082007aed4df31b5
mundix34 <mundix34@gmail.com>
2018-09-08 15:00:55 -0600

    detour from accordion

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,13 +1,13 @@
+var Collapse = require('rc-collapse');
+var Panel = Collapse.Panel;
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('rc-collapse/assets/index.css');
 
-import React, { Component } from 'react'
-
-export default class Article extends Component {
-  render() {
-  console.log("i am props match", this.props.match);
-    return (
-      <div>
-        <h1>Article</h1>
-      </div>
-    )
-  }
-}
+var collapse = (
+  <Collapse accordion={true}>
+    <Panel header="hello" headerClass="my-header-class">this is panel content</Panel>
+    <Panel header="title2">this is panel content2 or other</Panel>
+  </Collapse>
+);
+ReactDOM.render(collapse);
