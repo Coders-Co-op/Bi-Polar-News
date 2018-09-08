0fa8413e7e135a1dcd21f50d9df31e4fbad8b219
Jesse <rank_booster@yahoo.com>
2018-09-08 14:33:35 -0600

    Looking elsewhere

--- src/components/ReplaceSource.js
+++ src/components/ReplaceSource.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import _ from 'lodash'
+import {articles} from "./ducks/reducer";
+import {connect} from 'react-redux';
 
 export default (content, source) => {
     if (source === "CNN") {
         _.replace(content, "CNN", "[this news agency]");
+        _.replace(content, "Fox News", "[another news agency]");
+        _.replace(content, "FoxNews", "[another news agency]");
         _.replace(content, "Fox", "[another news agency");
-        _.replace(content, "Fox News", "[another news agency]")
-        _.replace(content, "FoxNews", "[another news agency]"),
-            _.replace(content, "FoxNews", "[another news agency]")
     } else {
         _.replace(content, "CNN", "[another news agency]");
-        _.replace(content, "Fox", "[this news agency")
-        _.replace(content, "Fox News", "[this news agency]")
-        _.replace(content, "FoxNews", "[this news agency]")
-        _.replace(content, "FoxNews", "[this news agency]")
+        _.replace(content, "Fox News", "[this news agency]");
+        _.replace(content, "FoxNews", "[this news agency]");
+        _.replace(content, "Fox", "[this news agency");
     }
     return (
         <span>
@@ -21,3 +21,17 @@ export default (content, source) => {
         </span>
     )
 }
+
+function mapStateToProps(state){
+    const {article} = state;
+    return { article }
+  }
+  export default connect(mapStateToProps, {article})()
+
+  function mapStateToProps(state) {
+    return {
+      user: state.users.user
+    };
+  }
+  
+  export default connect(mapStateToProps,{ updateUserData })(Private);
