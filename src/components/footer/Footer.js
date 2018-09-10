627debcc883427a3a1b2f1add17a0e48a8a5b5b4
Gregory Hart <gskhrvat@yahoo.com>
2018-09-10 15:02:00 -0600

    hiden footer with icons and only 2 articles show on Articles.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import "./footer.css";
 
 export default () => {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div class="icons">
+      <a href="#"><i class="fa fa-facebook-square"></i></a>
+      <a href="https://github.com/Coders-Co-op/Bi-Polar-News"><i class="fa fa-github"></i></a>
+      <a href="#"><i class="fa fa-twitter-square"></i></a>
     </div>
   )
 }
