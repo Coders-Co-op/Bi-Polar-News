5ba2e25268e55f9303f15540ce2e0acd6afb3d8b
mundix34 <mundix34@gmail.com>
2018-09-10 15:48:18 -0600

    detour from populating articles

--- src/route.js
+++ src/route.js
@@ -3,10 +3,10 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import About from './components/about/About';
 import Articles from './components/articles/Articles';
-// import ArticlesTwo from './components/articles/ArticlesTwo';
-// import ArticleThree from './components/articles/ArticlesThree';
-// import ArticlesFour from './components/articles/ArticlesFour';
-// import ArticlesFive from './components/articles/ArticlesFive';
+import ArticlesTwo from './components/articles/ArticlesTwo';
+import ArticlesThree from './components/articles/ArticlesThree';
+import ArticlesFour from './components/articles/ArticlesFour';
+import ArticlesFive from './components/articles/ArticlesFive';
 import Donate from './components/donate/Donate';
 import Intro from './components/intro/Intro';
 import Modal from './components/modal/Modal';
@@ -18,10 +18,10 @@ const routes = (
     <Switch>
         <Route exact path="/" component={Intro} />
         <Route path="/articles" component={Articles} />
-        {/* <Route path="/articles-2" component={ArticlesTwo} />
+        <Route path="/articles-2" component={ArticlesTwo} />
         <Route path="/articles-3" component={ArticlesThree} />
         <Route path="/articles-4" component={ArticlesFour} />
-        <Route path="/articles-5" component={ArticlesFive} /> */}
+        <Route path="/articles-5" component={ArticlesFive} />
         <Route path="/slider" component={Slider} />
         <Route path="/modal" component={Modal} />        
         <Route path="/photos" component={Photos} />
