a7894019681d1b124b54b62af57516e4510561d1
Sean <sean.b.white89@icloud.com>
2018-09-11 16:08:55 -0600

    added_about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,10 +1,50 @@
-
 import React from 'react'
+import './about.css'
 
 export default () => {
   return (
     <div>
-      <h1>About</h1>
+    <div className='wrapper'>
+    <h3>About Our Team</h3>
+      <div className="member">
+        <div className='row'>
+        <img src="https://via.placeholder.com/100x100" alt=""/>
+        <h3>Sean</h3>
+        </div>
+        <div className='floatright'>
+          <p>My Name is Sean I am a web dev living in spanish fork utah, I enjoy the outdoors</p>
+        </div>
+      </div>
+      <div className="member rowright">
+        <div className='row'>
+        <img src="https://via.placeholder.com/100x100" alt=""/>
+        <h3>Sean</h3>
+        </div>
+        <div className='floatright'>
+          <p>My Name is Sean I am a web dev living in spanish fork utah, I enjoy the outdoors</p>
+        </div>
+      </div>
+      <div className="member">
+        <div className='row'>
+        <img src="https://via.placeholder.com/100x100" alt=""/>
+        <h3>Sean</h3>
+        </div>
+        <div className='floatright'>
+          <p>My Name is Sean I am a web dev living in spanish fork utah, I enjoy the outdoors</p>
+        </div>
+      </div>
+      <div className="member rowright">
+        <div className='row'>
+        <img src="https://via.placeholder.com/100x100" alt=""/>
+        <h3>Sean</h3>
+        </div>
+        <div className='floatright'>
+          <p>My Name is Sean I am a web dev living in spanish fork utah, I enjoy the outdoors</p>
+        </div>
+      </div>
+    </div>
+    <div>
+    </div>
     </div>
   )
 }
