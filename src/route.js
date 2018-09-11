03c08fbaaa73d39f2c244863d06ded1d5e8d65b6
unknown <sean.b.white89@icloud.com>
2018-09-10 21:23:07 -0600

    added transition modal and styling

--- src/route.js
+++ src/route.js
@@ -1,21 +1,26 @@
 
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import About from './components/about/About';
 import Articles from './components/articles/Articles';
 import ArticlesTwo from './components/articles/ArticlesTwo';
 import ArticlesThree from './components/articles/ArticlesThree';
 import ArticlesFour from './components/articles/ArticlesFour';
 import ArticlesFive from './components/articles/ArticlesFive';
-import Donate from './components/donate/Donate';
 import Intro from './components/intro/Intro';
 import Modal from './components/modal/Modal';
 import Slider from './components/slider/Slider';
 import Topics from './components/topics/Topics';
 import Photos from './components/photos/Photos';
+import {AnimatedSwitch} from 'react-router-transition'
 
 const routes = (
-    <Switch>
+    <AnimatedSwitch
+        atEnter={{ opacity: 0 }}
+        atLeave={{ opacity: 0 }}
+        atActive={{ opacity: 1 }}
+        className="switch-wrapper"
+    >
         <Route exact path="/" component={Intro} />
         <Route path="/articles" component={Articles} />
         <Route path="/articles-2" component={ArticlesTwo} />
@@ -26,10 +31,8 @@ const routes = (
         <Route path="/modal" component={Modal} />        
         <Route path="/photos" component={Photos} />
         <Route path="/topics" component={Topics} />
-        <Route path="/about" component={About} />
-        <Route path="/donate" component={Donate} />
-
-    </Switch>
+        <Route path="/about" component={About} />        
+    </AnimatedSwitch>
 );
 
 export default routes
