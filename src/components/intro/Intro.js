e528d74539a497c1e9a720acc4c27cccda05104e
Jesse <rank_booster@yahoo.com>
2018-09-13 12:15:51 -0600

    various tweaks

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -16,7 +16,7 @@ export default () => {
           <div>
             <h1>Compare &amp; Contrast<br />
               Liberal and Conservative News Sources</h1>
-          <p>America's news has become so polarized that we have become “a nation divided” - Red vs Blue. So, to promote national unity, the BiPolar News gives you the chance to see both sides of major news stories.</p>
+          <p>America's news has become so <i>polarized</i> that we have become “a nation divided” -- Red vs Blue -- bi-polar. So, to promote national unity, the BiPolar News gives you the chance to see both sides of major news stories.</p>
 
           <p>This app presents you with two articles on major news topics, one from a premier conservative news source and the other from a premier liberal news source. The BiPolar News lets you read and compare, with an eye out for biased reporting. </p>
           </div>
